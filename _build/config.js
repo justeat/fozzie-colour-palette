2a27eccb8c60d360969900aaa0b8f204f749185c
ashleynolan <nolly00@gmail.com>
2017-05-26 13:08:01 +0100

    Setting up global colour palette as installable module :sunglasses: :rainbow:

---
+++ _build/config.js
@@ -0,0 +1,4 @@
+const srcDir = 'src';
+
+module.exports = {
+};
---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const gulp = require('gulp');
+const build = require('@justeat/gulp-build-fozzie');
+const config = require('./_build/config');
+
+build(gulp, config);
